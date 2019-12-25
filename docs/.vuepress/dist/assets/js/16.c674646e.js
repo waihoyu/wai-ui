522c29a3baf83b971c255299f481aff00c29c868
waihoyu <waihoyu0628@gmail.com>
2019-12-25 21:23:09 +0800

    docs update

---
+++ docs/.vuepress/dist/assets/js/16.c674646e.js
@@ -0,0 +1,4 @@
+;(window.webpackJsonp = window.webpackJsonp || []).push([
+    [16],
+    { 9: function(n, w, o) {} },
+])
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,5 @@
-# Hello VuePress!!!
+# M 站
+
+[官方文档](https://waihoyu.github.io/M1/)
+
+M 站 是一个不太一样的 UI 仿写库 。
